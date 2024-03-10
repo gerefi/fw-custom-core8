43bce95c45a8eb9f81b3e88c60531f6c3156b1ed
Bruno Ferreira <c_motorsport@hotmail.com>
2024-03-10 22:29:59 +0100

    new

---
+++ generated/controllers/generated/signature_core8.h
@@ -0,0 +1,6 @@
+//
+// was generated automatically by gerefi tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
+//
+
+#define SIGNATURE_HASH 23720420
+#define TS_SIGNATURE "gerefi default.2024.02.27.core8.23720420"
